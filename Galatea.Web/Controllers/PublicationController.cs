f5fe1e949a13a330c4ea76477796cf2d5c0e3d85
mariaIvanova-vn <maria_vn86@abv.bg>
2023-07-13 15:07:44 +0300

    Hide connection string

---
+++ Galatea.Web/Controllers/PublicationController.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Galatea.Web.Controllers
+{
+    [Authorize]
+    public class PublicationController : Controller
+    {
+       
+        [AllowAnonymous]
+        public async Task<IActionResult> All()
+        {
+            return View();
+        }
+    }
+}
