d24902e570aa18cf33cd95e4595cfb5c8708a6f3
mariaIvanova-vn <maria_vn86@abv.bg>
2023-07-21 21:59:49 +0300

    add All view

--- Galatea.Web/Controllers/PublicationController.cs
+++ Galatea.Web/Controllers/PublicationController.cs
@@ -1,7 +1,9 @@
 ﻿using Galatea.Services.Data.Interfaces;
+using Galatea.Services.Data.PublicationServiceModel;
 using Galatea.Web.ViewModels.Publication;
 using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
 
 namespace Galatea.Web.Controllers
 {
@@ -9,15 +11,25 @@ namespace Galatea.Web.Controllers
     public class PublicationController : Controller
     {
         private readonly ICategoryService _categoryService;
-        public PublicationController(ICategoryService categoryService)
+        private readonly IPublicationService _publicationService;
+
+        public PublicationController(ICategoryService categoryService, IPublicationService publicationService)
         {
             this._categoryService = categoryService;
+            this._publicationService = publicationService;
         }
-
+        [HttpGet]
         [AllowAnonymous]
-        public async Task<IActionResult> All()
+        public async Task<IActionResult> All([FromQuery] PublicationsAllQueryModel queryModel)
         {
-            return View();
+            AllPublicationFilteredServiceModel serviceModel =
+               await this._publicationService.AllAsync(queryModel);
+
+            queryModel.Publications = serviceModel.publications;
+            queryModel.PublicationsPerPage = serviceModel.TotalPublicationsCount;
+            queryModel.Categories = await _categoryService.AllCategoryNamesAsync();
+
+            return this.View(queryModel);
         }
 
         [HttpGet]
@@ -29,6 +41,37 @@ namespace Galatea.Web.Controllers
             };
 
             return View(formModel);
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> Add(PublicationFormModel formModel)
+        {
+            bool categoryExist = await _categoryService.ExistIdAsync(formModel.CategoryId);
+            if (!categoryExist)
+            {
+                ModelState.AddModelError(nameof(formModel.CategoryId), "Избраната категория не съществува!");
+            }
+
+
+            if (!ModelState.IsValid)
+            {
+                formModel.Categories = await _categoryService.GetAllCategoriesAsync();
+                return View(formModel);
+            }
+
+            try
+            {
+                await _publicationService.CreateAsync(formModel);
+                return RedirectToAction("All", "Publication");
+            }
+            catch (Exception)
+            {
+                ModelState.AddModelError(string.Empty, "Неочаквана грешка при добавянето на обява. Моля опитайте по-късно!");
+                formModel.Categories = await _categoryService.GetAllCategoriesAsync();
+                return View(formModel);
+            }
+
+            //return RedirectToAction("All", "Publication");
         }
     }
 }
