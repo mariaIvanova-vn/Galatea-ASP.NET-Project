402741f614031bbb1d74e459f2326cb17ecce4c5
mariaIvanova-vn <maria_vn86@abv.bg>
2023-07-27 12:46:21 +0300

    add Delete View for Publications

---
+++ Galatea.Web.ViewModels/Publication/PublicationDeleteDetailsViewModel.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Galatea.Web.ViewModels.Publication
+{
+    public class PublicationDeleteDetailsViewModel
+    {
+        public string Title { get; set; } = null!;
+
+        public string Content { get; set; } = null!;
+
+        public string ImageUrl { get; set; } = null!;
+    }
+}
