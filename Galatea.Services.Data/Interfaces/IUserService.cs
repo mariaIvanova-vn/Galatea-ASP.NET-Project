3b9dc71a8ccce68f300639403f49eb2677213262
mariaIvanova-vn <maria_vn86@abv.bg>
2023-07-27 12:45:24 +0300

    Add UserService

---
+++ Galatea.Services.Data/Interfaces/IUserService.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Galatea.Services.Data.Interfaces
+{
+    public interface IUserService
+    {
+        Task<string?> GetUserIdAsync();
+
+        Task<bool> IsUserWithIdOwnerOfPublicationWithIdAsync(string publicationId, string userId);
+    }
+}
---
+++ Galatea.Services.Data/UserService.cs
@@ -0,0 +1,39 @@
+﻿using Galatea.Data.Models;
+using Galatea.Services.Data.Interfaces;
+using Galatea.Services.Data.PublicationServiceModel;
+using Galatea.Web.Data;
+using Galatea.Web.ViewModels.Comments;
+using Galatea.Web.ViewModels.Publication;
+using Galatea.Web.ViewModels.Publication.Enum;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Logging;
+
+namespace Galatea.Services.Data
+{
+    public class UserService : IUserService
+    {
+        private readonly GalateaDbContext dbContext;
+
+        public UserService(GalateaDbContext dbContext)
+        {
+            this.dbContext = dbContext;
+        }
+
+        public async Task<string?> GetUserIdAsync()
+        {
+            var userId = await this.dbContext.UserClaims.Select(u=>u.UserId).ToArrayAsync();
+
+            return userId.ToString();
+        }
+
+        public async Task<bool> IsUserWithIdOwnerOfPublicationWithIdAsync(string publicationId, string userId)
+        {
+            Publication publication = await dbContext
+                .Publications
+                .Where(h => h.IsActive)
+                .FirstAsync(h => h.Id.ToString() == publicationId);
+
+            return publication.UserId.ToString() == userId;
+        }
+    }
+}
