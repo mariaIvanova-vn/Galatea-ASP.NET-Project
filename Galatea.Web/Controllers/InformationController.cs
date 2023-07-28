594cd930a90844d865cee68dba7032924aa75390
mariaIvanova-vn <maria_vn86@abv.bg>
2023-07-28 12:30:33 +0300

    add Information page

---
+++ Galatea.Web/Controllers/InformationController.cs
@@ -0,0 +1,35 @@
+﻿using Galatea.Web.ViewModels.Home;
+
+using Microsoft.AspNetCore.Mvc;
+using System.Diagnostics;
+
+namespace Galatea.Web.Controllers
+{
+    public class InformationController : Controller
+    {
+        public InformationController()
+        {
+        }
+
+        public IActionResult Information()
+        {
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error(int statusCode)
+        {
+            if (statusCode == 400 || statusCode == 404)
+            {
+                return View("Error404");
+            }
+
+            if (statusCode == 401)
+            {
+                return View("Error401");
+            }
+
+            return View();
+        }
+    }
+}
