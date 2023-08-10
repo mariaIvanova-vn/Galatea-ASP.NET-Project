0a54ff808ade37442388a2b796c22d93c6e5af8d
mariaIvanova-vn <maria_vn86@abv.bg>
2023-08-10 16:12:49 +0300

    add constants for administrator

--- Galatea.Common/GeneralConstants.cs
+++ Galatea.Common/GeneralConstants.cs
@@ -6,5 +6,9 @@
 
         public const int DefaultPage = 1;
         public const int DefaultPublicationPerPage = 5;
+
+        public const string AdminAreaName = "Admin";
+        public const string AdminRoleName = "Administrator";
+        public const string DevelopmentAdminEmail = "admin@admin.com";
     }
 }
